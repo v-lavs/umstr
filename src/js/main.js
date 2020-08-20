46ea53ccc235b8add234b043981430027a5d85e7
v-lavs <lavskavanda@gmail.com>
2020-08-20 23:18:13 +0300

    added layout

---
+++ src/js/main.js
@@ -0,0 +1,158 @@
+/**
+ *To include js file from libraries write: `//= include ./path-to-file`
+ * */
+
+
+
+/**
+ * CUSTOM SCRIPTS
+ **/
+
+$(document).ready(function () {
+    /**
+     * HEADER SCROLL
+     */
+
+    function onHeaderScrol() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 60) {
+            jQuery(".header").addClass('header_active');
+        } else {
+            jQuery(".header").removeClass('header_active');
+        }
+    }
+
+    /**
+     * MOB MENU SCRIPT
+     **/
+
+    var nav = $('.main-nav');
+
+    $('.burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn-close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+
+    // slider
+
+    var advantagesSlider;
+    var gymnasticSlider;
+    var questionsSlider;
+
+    function slidersInit() {
+        if ($(window).width() <= 1080) {
+          if (!advantagesSlider) {
+                advantagesSlider = new Swiper('#advantagesSlider', {
+                    slidesPerView: 1,
+                    breakpoints: {
+                        767: {
+                            slidesPerView: 2,
+                        },
+                    },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+                    navigation: {
+                        nextEl: '#advantagesSlider .swiper-button-next',
+                        prevEl: '#advantagesSlider .swiper-button-prev',
+                    },
+                });
+            }
+            if (!gymnasticSlider) {
+                gymnasticSlider = new Swiper('#gymnasticSlider', {
+                    slidesPerView: 1,
+                    breakpoints: {
+                        767: {
+                            slidesPerView: 2,
+                        },
+                    },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+                    navigation: {
+                        nextEl: '#gymnasticSlider .swiper-button-next',
+                        prevEl: '#gymnasticSlider .swiper-button-prev',
+                    },
+                });
+            }
+            if (!questionsSlider) {
+                questionsSlider = new Swiper('#questionsSlider', {
+                    slidesPerView: 1,
+                    breakpoints: {
+                        767: {
+                            slidesPerView: 2,
+                        },
+                    },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+                    navigation: {
+                        nextEl: '#questionsSlider .swiper-button-next',
+                        prevEl: '#questionsSlider .swiper-button-prev',
+                    },
+                });
+            }
+        } else {
+
+            if (advantagesSlider) {
+                advantagesSlider.destroy(true, true);
+                advantagesSlider = null;
+            }
+            if (gymnasticSlider) {
+                gymnasticSlider.destroy(true, true);
+                gymnasticSlider = null;
+            }
+            if (questionsSlider) {
+                questionsSlider.destroy(true, true);
+                questionsSlider = null;
+            }
+        }
+    }
+
+    slidersInit();
+
+    function setEqualHeight() {
+        var max_card_height = 0;
+        var $advantagesCards = $('.question-list__item');
+        $advantagesCards.each(function () {
+            if ($(this).height() > max_card_height) {
+                max_card_height = $(this).height();
+            }
+        });
+        $advantagesCards.css({minHeight: $(window).width() > 767 ? max_card_height : 'auto'});
+    }
+
+    setEqualHeight();
+
+    // PAIN SYMPTOMS Hover block
+
+    var $imagesList = $('#pain-syndrom-imgs .link-syndrom');
+    var $syndromsItems = $('.pain-syndrome-list__item');
+
+    $imagesList.hover(function () {
+        $syndromsItems.removeClass('active-syndrom');
+        $imagesList.removeClass('active');
+        $(this).addClass('active');
+        $('[data-id=' + $(this).data('for') + ']').addClass('active-syndrom');
+    });
+
+
+
+    $(window).on('resize', function () {
+        slidersInit();
+    });
+});
+
+
